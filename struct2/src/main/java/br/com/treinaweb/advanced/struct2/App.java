b916a83bdf25a8297b094af8e05542eb39c5c77b
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-09-05 21:40:51 -0300

    Struts2 - Maven configurado

---
+++ struct2/src/main/java/br/com/treinaweb/advanced/struct2/App.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.advanced.struct2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
